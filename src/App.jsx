a00c43d7e87ba5cc26d3595d2b5659d4a0dbecb5
Camillaatek <camillaatek@gmail.com>
2022-03-04 07:48:43 +0100

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>woop</h1>
+
+      {/*Header*/}
+
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      
+    </div>
+  );
+}
+
+export default App;
